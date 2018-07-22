02a518289e4d8d67716a3ef0423520c2ae9dc4d6
gap <khiemtonthat@yahoo.de>
2018-07-22 16:27:46 +0200

    added the BAR.vue

---
+++ src/components/github/Bar.vue
@@ -0,0 +1,30 @@
+
+<script>
+
+// CommitChart.js
+import {  Doughnut } from 'vue-chartjs'
+
+export default {
+  extends:  Doughnut,
+  props: ['data'],
+  mounted () {
+    // Overwriting base render method with actual data.
+    console.log(this.data)
+    this.renderChart({
+      labels: ['good', 'middle', 'bad'],
+      datasets: [
+        {
+          label: 'GitHub Commits',
+          backgroundColor: ['#66CC66', '#FFFF22', '#FF6633'],
+          data: [this.data[0], this.data[2], this.data[3]]
+        }
+      ]
+    })
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
